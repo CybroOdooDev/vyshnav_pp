6ee51ae29a1ded91d3482b796df073077f71d2d0
vyshnav22001 <vyshnav222001@gmail.com>
2023-08-24 16:51:45 +0530

    aug_24

--- odoo_jira_connector/controllers/odoo_jira_connector.py
+++ odoo_jira_connector/controllers/odoo_jira_connector.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
+
 from odoo import http
 from odoo.http import request
+
+_logger = logging.getLogger(__name__)
 
 
 class JiraWebhook(http.Controller):
     @http.route("/jira_webhook", type="json", auth="public", methods=['POST'],
                 csrf=False)
     def get_webhook_url(self, *args, **kwargs):
-        print('received webhook data')
+        _logger.info('received webhook data')
         try:
             data = json.loads(request.httprequest.data)
-            print(data)
+            _logger.info(data, "jira_data")
             return {'status': 'success'}
         except Exception as e:
             return {'status': 'error', 'error': str(e)}
